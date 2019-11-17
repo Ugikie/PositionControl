3e11a43497d3db0bc52d7541a21d8261ae2857b1
Austin Adam <austinadam42@gmail.com>
2019-11-16 17:23:40 -0800

    update verifyIfInPosition

--- Status/verifyIfInPosition.m
+++ Status/verifyIfInPosition.m
@@ -1,6 +1,6 @@
 function [axisInPosition] = verifyIfInPosition(MI4190,desiredPosition, positionError,verbose)
 %VERIFYIFINPOSITION Will notify the user when Axis (AZ) is in its desired pos.
-%       Queries the MI4190 every 2 seconds for its current
+%       Queries the MI4190 every 4 seconds for its current
 %       position, if it is at the desired position, then it breaks from the loop
 %       and notifies the user.
 if (nargin < 4)
